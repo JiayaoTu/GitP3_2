8c900f4704fd654f9629e194b3de9d529c9509a1
Kingtanna <15109256726@163.com>
2018-04-10 20:49:36 +0800

    practice03

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,21 @@
 package practice03;
 
 public class Worker {
+
+    String name;
+    int age;
+    public String getName() {return name;}
+    public int getAge(){return age;}
+
+    public Worker(){}
+    public Worker(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+    String introduce()
+    {
+        String str="I am a Worker. I have a job.";
+        return str;
+    }
 }
