e522c4399f0d0b7f5466f36d03c815aceeeeb022
Kingtanna <15109256726@163.com>
2018-04-10 21:21:34 +0800

    practice05

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,28 @@
 package practice05;
 
 public class Person {
+    String name;
+    int age;
+    public Person(){}
+
+    public Person(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+    public String getName() {return name;}
+    public int getAge(){return age;}
+
+    String introduce()
+    {
+        String str="My name is "+name+". "+"I am "+age+" years old.";
+        return str;
+    }
+
+    String PersoniItroduce()
+    {
+        String str="My name is "+name+". "+"I am "+age+" years old.";
+        return str;
+    }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    Person person;
+    public String getName() {return name;}
+    public int getAge(){return age;}
+
+    public Worker(){}
+    public Worker(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+    //String ss=person.PersoniItroduce();
+   // System.out.println(ss);
+    String introduce()
+    {
+       // String str="My name is "+name+"."+" I am "+age+" years old. I am a Worker. I have a job.";
+        String str=this.PersoniItroduce()+" I am a Worker. I have a job.";
+        return str;
+    }
 }
