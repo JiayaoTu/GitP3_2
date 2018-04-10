a6e54ef3bc6c98b861f58b6bc19f80c39eda027b
Kingtanna <15109256726@163.com>
2018-04-10 21:50:59 +0800

    practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,11 @@
 package practice07;
 
 public class Klass {
+    int number;
+    public Klass(){}
+    public Klass(int number){this .number=number;}
+    public int getNumber(){return number;}
+    String getDisplayName(){return "Class 2";}
+    Klass getKlass() {return new Klass();}
+
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,23 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    Klass klass;
+
+    public Klass getKlass() {return klass;}
+    public Student(){}
+    public Student(String name,int age,Klass klass)
+    {
+        this.name=name;
+        this.age=age;
+        this.klass=klass;
+    }
+    //String s=person.PersoniItroduce();
+
+    String introduce()
+    {
+        //String str="My name is "+name+"."+" I am "+age+" years old. I am a Student. I am at Class "+klass+".";
+
+        String str=this.PersoniItroduce()+" I am a Student. I am at Class "+klass.number+".";
+        return str;
+    }
 }
